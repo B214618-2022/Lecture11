1ea68aac79415bd341870ff4c56ba1e47df38e20
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-10-25 11:47:20 +0100

    adding a fruity script

---
+++ fruityscript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# import modules
+import os
+import sys
+
+# get input fromo the user
+first_fruit = sys.argv[1]
+second_fruit = sys.argv[2]
+
+print(first_fruit + " are just as nice as " + second_fruit)

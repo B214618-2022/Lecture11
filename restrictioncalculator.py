300a52fbc2232359cb63b5e822701e52c4a7f3a5
B214618-2022 <s1653324@ed.ac.uk>
2022-10-30 12:16:30 +0000

    starting fresh

--- restrictioncalculator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-#Written by B214618-2022 on 25/10/2022
-#-----------------------------------------
-
-my_dna = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
-restriction_site = "GAATTC"
-dna_length = len(my_dna)
-cut_position = my_dna.find("GAATTC") + 1
-print(str(dna_length - cut_position), str(cut_position))
